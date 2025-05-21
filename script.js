2ac114fdd67a1e3873d58bc9efa3493eb2fb35a9
joohyunah <sell050308@gmail.com>
2025-05-21 11:15:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function ledOn() {
+  console.log("LED ON")
+}
+
+function ledOff() {
+  console.log("LED OFF")
+}
+
+
+var config = {
+  apiKey: "AIzaSyBLg67U95hbfT8qMZhiGG3GxyAvcvpTyDo",
+  authDomain: "test1220-2a517.firebaseapp.com",
+  databaseURL: "https://test1220-2a517-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "test1220-2a517",
+  storageBucket: "test1220-2a517.firebasestorage.app",
+  messagingSenderId: "720434060430",
+  appId: "1:720434060430:web:2d9935a753466070e2bddf",
+}
